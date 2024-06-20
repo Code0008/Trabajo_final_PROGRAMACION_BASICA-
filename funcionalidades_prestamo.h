f7dd861dd68051750a789452315d4e3a1ec5821a
Gama <88944546+Code0008@users.noreply.github.com>
2024-06-20 04:09:56 -0500

    Update funcionalidades_prestamo.h

--- funcionalidades_prestamo.h
+++ funcionalidades_prestamo.h
@@ -9,23 +9,7 @@
 
 using namespace std;
 
-LIBRO inventario_disponible[15] = {
-	{202412340, 'L', "EL_QUIJOTE_DE_LA_MANCHA", false},
-	{202414555, 'L', "CIEN_AÑOS_DE_SOLEDAD", false},
-	{202417888, 'L', "DON_QUIJOTE", false},
-	{202420111, 'L', "LA_CASA_DE_LOS_ESPIRITUS", false},
-	{202421222, 'L', "1984", false},
-	{202424555, 'L', "LA_RUTA_DEL_TILIN", false},
-	{202413830, 'R', "NAT_GEO_NEW_PUBLICATION", false},
-	{202415666, 'R', "TIME_MAGAZINE", false},
-	{202418999, 'R', "SCIENTIFIC_AMERICAN", false},
-	{202422333, 'R', "FORBES", false},
-	{202425666, 'R', "THE_ECONOMIST", false},
-	{202412334, 'P', "ABEJA_REPUBLICANA", false},
-	{202416777, 'P', "NEW_YORK_TIMES", false},
-	{202419000, 'P', "WASHINGTON_POST", false},
-	{202423444, 'P', "THE_GUARDIAN", false}
-};
+
 
 extern void see_inventory() {
 	cout << endl;
@@ -54,13 +38,13 @@ extern void see_inventory() {
 
 }
 
-static bool verif_codigo_libro(int usuario_id, string codigo_libro) {
+static bool verif_codigo_libro(int cliente_id, string codigo_libro) {
 	int n = sizeof(inventario_disponible) / sizeof(inventario_disponible[0]);
 	if (verif_entero(codigo_libro)) {
 		if (codigo_libro.length() == 9) {
 			for (int i = 0; i < n; i++) {
 				if (inventario_disponible[i].codigo_libro == stoi(codigo_libro) && inventario_disponible[i].prestado == false) {
-					append_libro(usuario_id, codigo_libro, "prestamo");
+					append_libro(cliente_id, codigo_libro, "prestamo");
 					inventario_disponible[i].prestado = true;
 					return true;
 				}
@@ -71,20 +55,20 @@ static bool verif_codigo_libro(int usuario_id, string codigo_libro) {
 }
 
 
-static  string calculo_dia_devolucion(int usuario_id) {
+static  string calculo_dia_devolucion(int cliente_id) {
 	int dias_max=0;
 	int n1 = sizeof(inventario_disponible) / sizeof(inventario_disponible[0]);
 
 	int indice = 0;
 
 	for (int i = 0; i < 50; i++) {
-		if (usuarios[usuario_id].libros_tratados[i][3] == "true" && usuarios[usuario_id].libros_tratados[i + 1][3] == "void") {
+		if (clientes[cliente_id].libros_tratados[i][3] == "true" && clientes[cliente_id].libros_tratados[i + 1][3] == "void") {
 			indice = i;
 			break;
 		}
 	}
 	for (int i = 0; i < n1; i++) {
-		if (inventario_disponible[i].codigo_libro == stoi(usuarios[usuario_id].libros_tratados[indice][0])) {
+		if (inventario_disponible[i].codigo_libro == stoi(clientes[cliente_id].libros_tratados[indice][0])) {
 			if (inventario_disponible[i].tipo_libro = 'L' || inventario_disponible[i].tipo_libro == 'R') {
 				dias_max = 5;
 				break;
@@ -113,34 +97,34 @@ static  string calculo_dia_devolucion(int usuario_id) {
 	return tiempo_capturado;
 }
 
-extern void generar_matris(int usuario_id = 0) {
+extern void generar_matris(int cliente_id = 0) {
 	for (int i = 0; i < 50; i++) {
-		usuarios[usuario_id].libros_tratados[i] = new string[4];
+		clientes[cliente_id].libros_tratados[i] = new string[4];
 	}
 	for (int i = 0; i < 50; i++) {
-		usuarios[usuario_id].libros_tratados[i][3] = "void";
+		clientes[cliente_id].libros_tratados[i][3] = "void";
 	}
 }
 
-extern  void see_matris(int usuario_id = 0) {
+extern  void see_matris(int cliente_id = 0) {
 	for (int i = 0; i < 50; i++) {
-		if (usuarios[usuario_id].libros_tratados[i][3] == "true") {
+		if (clientes[cliente_id].libros_tratados[i][3] == "true") {
 			cout << "\t";
 			for (int j = 0; j < 4; j++) {
-				cout << usuarios[usuario_id].libros_tratados[i][j] << "|";
+				cout << clientes[cliente_id].libros_tratados[i][j] << "|";
 			} cout << endl;
 		}
 	}
 }
 
-extern void obtener_informacion_prestamo(int usuario_id = 0) {
+extern void obtener_informacion_prestamo(int cliente_id = 0) {
 	
 	string var_to_game;
-	if (usuarios[usuario_id].DNI == "void") {
+	if (clientes[cliente_id].DNI == "void") {
 		while (true) {
 			cout << "\n\t[+]Ingrese su numero de DNI: "; getline(cin, var_to_game);
 			if (verif_dni(var_to_game)) {
-				usuarios[usuario_id].DNI = var_to_game; break;
+				clientes[cliente_id].DNI = var_to_game; break;
 			}
 			else { cout << RED << "\t[!]" << RESET << ORANGE << " Ingrese bien su DNI!\n" << RESET; }
 		}
@@ -149,31 +133,31 @@ extern void obtener_informacion_prestamo(int usuario_id = 0) {
 	while (true) {
 		var_to_game = "";
 		cout << "\n\t[+]Ingrese el codigo del libro: "; getline(cin, var_to_game);
-		if (verif_codigo_libro(usuario_id,var_to_game)) {
+		if (verif_codigo_libro(cliente_id,var_to_game)) {
 			break;
 		}
 		else {
 			cout << RED << "\t[!]" << RESET << ORANGE << " Libro prestado o codigo invalido!\n" << RESET;
 		}
 	}
-	cout << "\t[!] El dia maximo para devolver es: " << calculo_dia_devolucion(usuario_id) << endl;
+	cout << "\t[!] El dia maximo para devolver es: " << calculo_dia_devolucion(cliente_id) << endl;
 }
 
 
-static void boleta_prestamo(int usuario_id = 0) {
+static void boleta_prestamo(int cliente_id = 0) {
 	estetica(100, '_'); cout << endl;
-	cout << "\t" <<(char)186; estetica(10, ' '); cout <<"ID USUARIO" << (char)175 <<" " << usuarios[usuario_id].userID << endl;
-	cout << "\t" << (char)186; estetica(10, ' '); cout <<"DNI" << (char)175 << " " << usuarios[usuario_id].DNI << endl;
-	cout << "\t" << (char)186; estetica(10, ' '); cout <<"NOMBRE" << (char)175 <<" " << usuarios[usuario_id].NOMBRE_APELLIDO << endl;
-	cout << "\t" << (char)186; estetica(10, ' '); cout <<"EDAD" <<(char)175 << " "<<usuarios[usuario_id].EDAD << endl;
-	cout << "\t" << (char)186; estetica(10, ' '); cout <<"SEXO" << (char)175 <<" "<< usuarios[usuario_id].SEXO << endl;
-	cout << "\t" << (char)186; estetica(10, ' '); cout << "TIPO DOMICILIO" << (char)175 <<" " << usuarios[usuario_id].direccion.CASA_DEPARTAMENTO << endl;
-	cout << "\t" << (char)186; estetica(10, ' '); cout << "DIRECCION" << (char)175 << " " << usuarios[usuario_id].direccion.DIRECCION << endl;
-	cout << "\t" << (char)186; estetica(10, ' '); cout << "DISTRITO" << (char)175 <<" " << usuarios[usuario_id].direccion.DISTRITO << endl;
-	cout << "\t" << (char)186; estetica(10, ' '); cout <<"PROVINCIA" << (char)175 <<" "<< usuarios[usuario_id].direccion.PROVINCIA << endl;
-	cout << "\t" << (char)186; estetica(10, ' '); cout <<"NUMERO DE CELULAR" << (char)175 <<" " << usuarios[usuario_id].CELULAR << endl;
-	cout << "\t" << (char)186; estetica(10, ' '); cout << "HORA DE INGRESO" << (char)175<<" " << usuarios[usuario_id].hora_operacion << endl;
+	cout << "\t" <<(char)186; estetica(10, ' '); cout <<"ID USUARIO" << (char)175 <<" " << clientes[cliente_id].userID << endl;
+	cout << "\t" << (char)186; estetica(10, ' '); cout <<"DNI" << (char)175 << " " << clientes[cliente_id].DNI << endl;
+	cout << "\t" << (char)186; estetica(10, ' '); cout <<"NOMBRE" << (char)175 <<" " << clientes[cliente_id].NOMBRE_APELLIDO << endl;
+	cout << "\t" << (char)186; estetica(10, ' '); cout <<"EDAD" <<(char)175 << " "<<clientes[cliente_id].EDAD << endl;
+	cout << "\t" << (char)186; estetica(10, ' '); cout <<"SEXO" << (char)175 <<" "<< clientes[cliente_id].SEXO << endl;
+	cout << "\t" << (char)186; estetica(10, ' '); cout << "TIPO DOMICILIO" << (char)175 <<" " << clientes[cliente_id].direccion.CASA_DEPARTAMENTO << endl;
+	cout << "\t" << (char)186; estetica(10, ' '); cout << "DIRECCION" << (char)175 << " " << clientes[cliente_id].direccion.DIRECCION << endl;
+	cout << "\t" << (char)186; estetica(10, ' '); cout << "DISTRITO" << (char)175 <<" " << clientes[cliente_id].direccion.DISTRITO << endl;
+	cout << "\t" << (char)186; estetica(10, ' '); cout <<"PROVINCIA" << (char)175 <<" "<< clientes[cliente_id].direccion.PROVINCIA << endl;
+	cout << "\t" << (char)186; estetica(10, ' '); cout <<"NUMERO DE CELULAR" << (char)175 <<" " << clientes[cliente_id].CELULAR << endl;
+	cout << "\t" << (char)186; estetica(10, ' '); cout << "HORA DE INGRESO" << (char)175<<" " << clientes[cliente_id].hora_operacion << endl;
 	estetica(100, 238); cout << endl;
-	see_matris(usuario_id);
+	see_matris(cliente_id);
 
 }
