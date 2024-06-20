b89aba7667f3e325f995970206e9c050a5585039
Gama <88944546+Code0008@users.noreply.github.com>
2024-06-20 04:09:08 -0500

    Update STRUCTURAS.h

--- STRUCTURAS.h
+++ STRUCTURAS.h
@@ -18,8 +18,14 @@ struct direccion_usuario {
 	string PROVINCIA = "LIMA";
 	string CASA_DEPARTAMENTO = "void";
 };
+struct almacenamiento_matrices {
+	string** almacenamiento = new string * [100];
+};
 
-struct USUARIO {
+struct correo {
+	string correo_contacto;
+};
+struct CLIENTE {
 	int userID = 0;
 	string DNI = "void";
 	string NOMBRE_APELLIDO;
@@ -28,8 +34,9 @@ struct USUARIO {
 	direccion_usuario direccion;
 	string CELULAR = "void";
 	string hora_operacion = "void";
-	string correo = "void";
-	string** libros_tratados = new string*[100];
+	correo correo_cliente;
+	string** libros_tratados = new string*[100]; //inventario
+	almacenamiento_matrices log_de_cliente;
 	bool tiene_sancion=false;
 	string tiempo_sancion = "NO PRESENTA";
 	/*
@@ -40,16 +47,48 @@ struct USUARIO {
 	*/
 };
 
-struct ADMINISTRADOR {
 
+
+
+struct USUARIO {
 	string nombre_adminsitrador;
 	string password;
 	string hora_acceso;
-	string correo_contacto;
+	correo correo_usuario;
 	bool see_reportes = false;
 	bool adm_users = false;
+	almacenamiento_matrices mensajes_usuario;
+	almacenamiento_matrices log_usuario;
 	/*
 	Ver reportes | Desbloquear/Bloquear usuarios |
 	|     V/F    | V/F							 |
 	*/
+	/*
+	* 0				1						2				3					
+	|indice_usuario| hora de ejecucion | proceso_ejecucion | verifiacdor
+	*/
+};
+extern USUARIO usuarios[2] = {
+	{"EnzoFavito", "MtqfRzsit", "void", "enzofavito@biblio.com", true, true},
+	{"RalphSito", "Xmfnijq","void", "ralphxshaiel@teamoshaiel.com", true, false}
+};
+LIBRO* inventario = new LIBRO[100];
+
+CLIENTE* clientes = new CLIENTE[100];
+LIBRO inventario_disponible[15] = {
+	{202412340, 'L', "EL_QUIJOTE_DE_LA_MANCHA", false},
+	{202414555, 'L', "CIEN_AÑOS_DE_SOLEDAD", false},
+	{202417888, 'L', "DON_QUIJOTE", false},
+	{202420111, 'L', "LA_CASA_DE_LOS_ESPIRITUS", false},
+	{202421222, 'L', "1984", false},
+	{202424555, 'L', "LA_RUTA_DEL_TILIN", false},
+	{202413830, 'R', "NAT_GEO_NEW_PUBLICATION", false},
+	{202415666, 'R', "TIME_MAGAZINE", false},
+	{202418999, 'R', "SCIENTIFIC_AMERICAN", false},
+	{202422333, 'R', "FORBES", false},
+	{202425666, 'R', "THE_ECONOMIST", false},
+	{202412334, 'P', "ABEJA_REPUBLICANA", false},
+	{202416777, 'P', "NEW_YORK_TIMES", false},
+	{202419000, 'P', "WASHINGTON_POST", false},
+	{202423444, 'P', "THE_GUARDIAN", false}
 };
