d747a4438a6e7ecddc5cae36fcbaf4f8d26e207f
Gama <88944546+Code0008@users.noreply.github.com>
2024-06-27 00:08:39 -0500

    Update administradores.h

--- administradores.h
+++ administradores.h
@@ -8,9 +8,12 @@
 #include "COLORAMA.h"
 #include "FUNCIONALIDADES.h"
 #include "Funcionalidad_reporte_grafico_de_barras.h"
+#include "reporte_prestamos_por_tipo_de_publi.h"
 #include <string>
-#include <cctype>
 #include "maker_logs.h"
+#include "REPORTE PUBLICACIONES QUE SE PASARON TIEMPO DE PUBLICACION.h"
+#include "reporte_rango_de_edad.h"
+#include "administrar_usuarios.h"
 using namespace std;
 
 
@@ -89,27 +92,32 @@ char  seleccion_reporte(int indice_usuario, string comando) {
     if (comando == "lectores") {
         return 'l';
     }
+    if (comando == "cls") {
+        return 'c';
+    }
     return 'x';
 }
 
 static void ingresar_a_reportes(int indice_usuario) {
     system("cls");
-    cin.ignore();
     system("cls");
-    cout << endl;
-    cout << "\t[+] Bienvendio " << usuarios[indice_usuario].nombre_adminsitrador << endl;
-    cout << "\t[+] Por favor ingrese el reporte que quiere generar:" << endl;
-    cout << "\t[+] COMANDOS" << endl;
-
+    cout << endl<<endl<<endl<<endl;
+    cout << MAGENTA<<"\t\t\t\t"; estetica(97, 254); cout << endl;
+    cout << LGREEN << "\t\t\t\t\t[!]" << CYAN << "BIENVENDIO " << usuarios[indice_usuario].nombre_adminsitrador <<RESET<< endl;
+    cout << ORANGE<<"\t\t\t\t\t[+]" << CYAN << " Por favor ingrese el reporte que quiere generar:" << endl;
+    cout << "\t\t\t\t\t[+] " << ROSE << "COMANDOS" << endl;
+    char seleccion;
+    string var_game;
     string comando;
     while (true) {
-        cout << endl;
-        cout << "\t[!] grafico barras -> grafico" << endl;
-        cout << "\t[!] porcentaje de prestamo -> porcentaje" << endl;
-        cout << "\t[!] pendientes de devolcion -> pendientes" << endl;
-        cout << "\t[!] lectores de rango de edad -> lectores" << endl;;
-        cout << "\t[!!] exit -> exit" << endl;
-        cout << "\t[+] -- > "; getline(cin, comando);
+        cout << MAGENTA<<"\t\t\t\t";  estetica(97, 254); cout << RESET << endl;
+        cout << LGREEN << "\t\t\t\t\t[!]" << CYAN << " grafico barras ->"<<ROSE<<" grafico" << endl;
+        cout << LGREEN << "\t\t\t\t\t[!]" << CYAN << " porcentaje de prestamo -> " << ROSE << "porcentaje" << endl;
+        cout << LGREEN << "\t\t\t\t\t[!]" << CYAN << " pendientes de devolcion -> " << ROSE << "pendientes" << endl;
+        cout << LGREEN<<"\t\t\t\t\t[!]" << CYAN << " lectores de rango de edad ->" << ROSE << " lectores" << endl;;
+        cout << LGREEN << "\t\t\t\t\t[!!]" << ROSE << " limpiar pantalla -> cls" << endl;
+        cout << RED<<"\t\t\t\t\t[!!]" << ORANGE << " exit -> exit" << endl;
+        cout << LGREEN << "\t\t\t\t\t[+]"<<ORANGE<<" -- > "<<RESET; getline(cin, comando);
         if (comando == "exit") { 
             break; 
         }
@@ -117,17 +125,47 @@ static void ingresar_a_reportes(int indice_usuario) {
             switch (seleccion_reporte(indice_usuario, comando))
             {
             case 'g':
-                grafico_de_barras();
-                break;
+                while (true) {
+                    system("cls");
+                    cout << endl << endl<<endl;
+                    cout << "\t\t\t\t" << MAGENTA; estetica(97, 219); cout << endl;
+                    cout << LGREEN << "\t\t\t\t\t[!]" << CYAN << " GRAFICO PRIMER SEMESTRE " << ROSE << "[ 1 ]" << endl;
+                    cout << LGREEN << "\t\t\t\t\t[!]" << CYAN << " GRAFICO SEGUNDO SEMESTRE " << ROSE << "[ 2 ]" << endl;
+                    cout << YELLOW << "\t\t\t\t\t--> "; seleccion = _getch(); cout << endl; 
+                    if (seleccion == '1' || seleccion == '2') {
+                        cout << LGREEN << "\t\t\t\t\t[!]" << CYAN << "ELIGE UN AÑO ENTRE " << ROSE << "(2017-2024) :" <<RESET; getline(cin,var_game); 
+                        if (verif_entero(var_game) && stoi(var_game)>=2017 && stoi(var_game)<=2024) {
+                            system("cls");
+                            cout << endl << endl << endl;
+                            cout << "\t\t\t\t" << CYAN; estetica(97, 219); cout << endl;
+                            grafico_de_barras(seleccion, stoi(var_game)); cout << endl;
+                            break;
+                        }
+                    }
+                    else { cout << RED << "\t\t\t\t\t[!] " << RESET << ORANGE << "INGRESE BIEN LA INFORMACION POR FAVOR" << RESET << endl; }
+                }break;
             case 'p':
-                cout << "porcentaje"; break;
+                call_reporte_prestmos(); break;
             case 'e':
-                cout << "pendientes"; break;
+                cout << endl;
+                cout << "\t\t\t\t" << MAGENTA; estetica(97, 219); cout << endl;
+                cout << LGREEN << "\t\t\t\t\t[!]" << CYAN << "SE MOSTRARAN LOS CODIGOS DE LAS PUBLICACIONES FUERA DE TIEMPO" << ROSE << " AÑO ACTUAL";;
+
+                principal_reporte_no_devueltos(); break;
             case 'l':
-                cout << "lectores"; break;
+                cout << LGREEN << "\t\t\t\t\t[!]" << CYAN << "ELIGE UN AÑO ENTRE " << ROSE << "(2017-2024) :" << RESET; getline(cin, var_game);
+                if (verif_entero(var_game) && stoi(var_game) >= 2017 && stoi(var_game) <= 2024) {
+                    system("cls");
+                    cout << endl << endl << endl;
+                    cout << LGREEN << "\t\t\t\t\t\t\t\t[!]" << ROSE << " REPORTE DE CLIENTES EN RANGO DE EDAD " << RESET << endl;
+                    cout << "\t\t\t\t" << CYAN; estetica(97, 219); cout << endl;
+                    principal_reporte_de_lectores_edad(stoi(var_game)); cout << endl;
+                    break;
+                } break;
+            case 'c': system("cls"); break;
             }
         }else {
-            cout << RED << "\t[!] " << RESET << ORANGE << "DISCULPE NO PUEDE INGRESAR" << RESET << endl;
+            cout << RED << "\t\t\t\t\t[!] " << RESET << ORANGE << "DISCULPE NO PUEDE INGRESAR" << RESET << endl;
         }
     }
 
@@ -136,7 +174,6 @@ static void ingresar_a_reportes(int indice_usuario) {
 
 extern void informacion_administrador() {
     string var_to_game;
-    cin.ignore();
     int indice = 0;
     bool is_valid = false;
     int intentos = 0;
@@ -145,56 +182,66 @@ extern void informacion_administrador() {
         if (intentos == 3) {
             break;
         }
+        cout << endl<<BLUE<<"\t\t\t\t\t"; estetica(49, 219); cout << endl;
         cout << endl;
-        cout << ROSE "\t[+]" << RESET << LGREEN "ESCRIBA EXIT SI SE EQUIVOCO " << RESET<<endl;
-        cout << ROSE "\t[+]" << RESET << LGREEN "Ingrese su nombre de administrativo: " << RESET;
+        cout << ROSE "\t\t\t\t\t\t[+]" << RESET << LGREEN "ESCRIBA EXIT SI SE EQUIVOCO " << RESET<<endl;
+        cout << ROSE "\t\t\t\t\t\t[+]" << RESET << LGREEN "Ingrese su nombre de administrativo: " << RESET;
         getline(cin, var_to_game);
         if (var_to_game == "exit") {
             break;
         }
         indice = valid_username(var_to_game);
         if (indice < 8) {
-            cout << ROSE<<"\t[!]"<< RESET<< LGREEN<<"Ingrese su contrasena : " << RESET;
-            intentos++;
+            cout << ROSE<<"\t\t\t\t\t\t[!]"<< RESET<< LGREEN<<"Ingrese su contrasena : " << RESET;
+         
             getline(cin, var_to_game);
             if (validate_exist_passwor(var_to_game, indice)) {
                 is_valid = true;
                 break;
             }
             else {
-                cout << RED << "\t[!] " << RESET << ORANGE << " CONTRASEÑA MAL INGRESADA!" << RESET << endl;
+                intentos++;
+                cout << RED << "\t\t\t\t\t\t[!] " << RESET << ORANGE << " CONTRASEÑA MAL INGRESADA!" << RESET << endl;
                 continue;
             }
         }
-        else { cout << RED << "\t[!] " << RESET << ORANGE  <<" USUARIO NO EXISTE" << RESET<< endl; continue; }
+        else { intentos++; cout << RED << "\t\t\t\t\t\t[!] " << RESET << ORANGE  <<" USUARIO NO EXISTE" << RESET<< endl; continue; }
+  
     }
     if (is_valid){
+        system("cls");
         crear_logs_usuario(indice);
-
-        cout << "\t[!] BIENVENDIO " << usuarios[indice].nombre_adminsitrador << endl;
-        cout << "\t[!] hora de acceso " << usuarios[indice].hora_acceso << endl;
-        cout << "\t[+] privilegios: " << endl;
-        cout << "\t Ver reportes: " << bool(usuarios[indice].see_reportes) << endl;
-        cout << "\t Administrar usuarios y clientes: " << bool(usuarios[indice].adm_users) << endl;
-        char selec;
+        cout << endl << endl << endl;
 
         while (true) {
+        cout << MAGENTA << "\t\t\t\t\t";  estetica(97, 254); cout << RESET << endl;
+        cout <<LGREEN <<"\t\t\t\t\t[!]" << CYAN<< "BIENVENDIO "<<ORANGE << usuarios[indice].nombre_adminsitrador << endl;
+        cout << LGREEN<<"\t\t\t\t\t[!]" << CYAN << " hora de acceso " << usuarios[indice].hora_acceso << endl;
+        cout <<LGREEN<< "\t\t\t\t\t[+]" << CYAN << " privilegios: " << endl;
+        cout << LGREEN<<"\t\t\t\t\t[1]" << CYAN << " Ver reportes: " << bool(usuarios[indice].see_reportes) << endl;
+        cout <<LGREEN<< "\t\t\t\t\t[2]" << CYAN << " Administrar usuarios y clientes: " << bool(usuarios[indice].adm_users) << endl;
+        cout << RED << "\t\t\t\t\t[!!]" << ORANGE << " salir -> exit" << endl;
+        char selec;
             while (true) {
                 cout << endl;
-                cout << "\t--> ";
-                cin >> selec; selec = toupper(selec);
-                if (selec == 'A' || selec == 'R') {
+                cout << ORANGE<<"\t\t\t\t\t--> "<<RESET;
+                selec = _getch();
+                selec = toupper(selec);
+                if (selec == 'A' || selec == 'R'||selec == 'X') {
                     break;
                 }
 
             }
+            if (selec == 'X') {
+                break;
+            }
             switch (selec)
             {
             case 'A': if (usuarios[indice].adm_users != false) {
-                ingresar_a_reportes(indice);
+                main_administrar_cliente(indice);
             }
                     else {
-                cout << RED << "\t[!] " << RESET << ORANGE << "DISCULPE NO PUEDE INGRESAR" << RESET << endl;
+                cout << RED << "\t\t\t\t\t[!]" << RESET << ORANGE << " NO TIENE PERMISO PARA ACCEDER\n" << RESET;
             } break;
             case 'R':
                 if (usuarios[indice].see_reportes != false) {
@@ -202,7 +249,7 @@ extern void informacion_administrador() {
                 }
                 else {
                     cout << endl;
-                    cout << RED << "\t[!] " << RESET << ORANGE << "NO PUEDE INGRESAR" << RESET << endl;
+                    cout << RED << "\t\t\t\t\t[!]" << RESET << ORANGE << " N0 TIENE PERMISO PARA ACCEDER\n" << RESET;
                 }
                 break;
             default:
@@ -211,10 +258,6 @@ extern void informacion_administrador() {
         }
 
     }
-
+    system("cls");
 }
-
-
-
-
 
