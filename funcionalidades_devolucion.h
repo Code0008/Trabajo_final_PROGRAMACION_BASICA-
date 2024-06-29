87af14be659d18214176e9f4fc44a894184e22e9
Gama <88944546+Code0008@users.noreply.github.com>
2024-06-28 21:38:33 -0500

    Update funcionalidades_devolucion.h

--- funcionalidades_devolucion.h
+++ funcionalidades_devolucion.h
@@ -1,31 +1,15 @@
 #pragma once
-#include <iostream>
-#include <stdio.h>
 #include "FUNCIONALIDADES.h"
-#include <string>
-#include <string>
-#include <cctype>
-#include <ctime>
+#include "cabecera_principales.h"
 
 
 using namespace std;
 
 bool verif_date(string date_string, int menos_dias = 0, int cliente_id = 0) {
-
-
     switch (date_string.length()){
     case 9:  break;
     case 10: break;
     default: return false; break;
-    }
-    int count_verif_ = 0;
-    for (int i = 0; i < date_string.length(); i++) {
-        if (date_string[i] == '-') {
-            count_verif_++;
-        }
-    }
-    if (count_verif_ != 2) {
-        return false;
     }
     bool verife = false;
     string year = "";
@@ -76,7 +60,7 @@ bool verif_date(string date_string, int menos_dias = 0, int cliente_id = 0) {
         }
     }
 
-    if (stoi(moth) < 7 && stoi(day) < 31 && stoi(day)<= tiempo.tm_mday) {
+    if (stoi(moth) <= 7 && stoi(day) < 31 && stoi(day)<= tiempo.tm_mday) {
         fecha_real += year;
         fecha_real += current;
         fecha_real += day;
@@ -126,11 +110,11 @@ static bool verif_codigo_libro_devolucion(string codigo_libro) {
 extern void obtener_informacion_devolucion(int cliente_id = 0) { // veces_ejecutado para filas
     string var_to_game;
     cout << endl;
-    if (clientes[cliente_id].informacion_cliente.DNI == "void") {
+    if (clientes[cliente_id].DNI == "void") {
         while (true) {
             cout << LGREEN << "\t\t\t\t\t[!]" << RESET << CYAN << " INGRESE SU DNI: " << RESET;  getline(cin, var_to_game);
             if (verif_dni(var_to_game)) {
-                clientes[cliente_id].informacion_cliente.DNI = var_to_game; break;
+                clientes[cliente_id].DNI = var_to_game; break;
             }
             else { cout << RED << "\t\t\t\t\t[!]" << RESET << ORANGE << " Ingrese bien su DNI!\n" << RESET; }
         }
@@ -150,7 +134,7 @@ extern void obtener_informacion_devolucion(int cliente_id = 0) { // veces_ejecut
     int tipo_publi_dias = 0;
     while (true)
     {
-        cout << LGREEN << "\t\t\t\t\t[!]" << RESET << CYAN << " INGRESE TIPO DE PUBLICACION (R)EVISTA/(L)IBRO/(): " << RESET;
+        cout << LGREEN << "\t\t\t\t\t[!]" << RESET << CYAN << " INGRESE TIPO DE PUBLICACION: " << RESET;
         tipo_publi = _getch();
         tipo_publi = toupper(tipo_publi);
         if (tipo_publi == 'R' || tipo_publi == 'L' || tipo_publi == 'P') {
@@ -189,15 +173,15 @@ extern void boleta_devolucion(int cliente_id = 0) {
     cout << "\t\t\t\t\t\t\t\t" << LGREEN << "BOLETA FINAL DEL USUARIO" << RESET << endl;
     cout << "\t\t\t\t\t"; estetica(65, '_'); cout << endl;
     cout << "\t\t\t\t\t\t" << (char)186; estetica(7, ' '); cout << CYAN << "ID USUARIO" << (char)175 << " " << clientes[cliente_id].userID << RESET << endl;
-    cout << "\t\t\t\t\t\t" << GRAY << (char)186; estetica(7, ' '); cout << LGREEN << "DNI" << (char)175 << " " << clientes[cliente_id].informacion_cliente.DNI << RESET << endl;
-    cout << "\t\t\t\t\t\t" << GRAY << (char)186; estetica(7, ' '); cout << LGREEN << "NOMBRE" << (char)175 << " " << clientes[cliente_id].informacion_cliente.NOMBRE_APELLIDO << RESET << endl;
-    cout << "\t\t\t\t\t\t" << GRAY << (char)186; estetica(7, ' '); cout << LGREEN << "EDAD" << (char)175 << " " << clientes[cliente_id].informacion_cliente.EDAD << RESET << endl;
-    cout << "\t\t\t\t\t\t" << GRAY << (char)186; estetica(7, ' '); cout << LGREEN << "SEXO" << (char)175 << " " << clientes[cliente_id].informacion_cliente.SEXO << RESET << endl;
+    cout << "\t\t\t\t\t\t" << GRAY << (char)186; estetica(7, ' '); cout << LGREEN << "DNI" << (char)175 << " " << clientes[cliente_id].DNI << RESET << endl;
+    cout << "\t\t\t\t\t\t" << GRAY << (char)186; estetica(7, ' '); cout << LGREEN << "NOMBRE" << (char)175 << " " << clientes[cliente_id].NOMBRE_APELLIDO << RESET << endl;
+    cout << "\t\t\t\t\t\t" << GRAY << (char)186; estetica(7, ' '); cout << LGREEN << "EDAD" << (char)175 << " " << clientes[cliente_id].EDAD << RESET << endl;
+    cout << "\t\t\t\t\t\t" << GRAY << (char)186; estetica(7, ' '); cout << LGREEN << "SEXO" << (char)175 << " " << clientes[cliente_id].SEXO << RESET << endl;
     cout << "\t\t\t\t\t\t" << GRAY << (char)186; estetica(7, ' '); cout << LGREEN << "TIPO DOMICILIO" << (char)175 << " " << clientes[cliente_id].direccion.CASA_DEPARTAMENTO << RESET << endl;
     cout << "\t\t\t\t\t\t" << GRAY << (char)186; estetica(7, ' '); cout << LGREEN << "DIRECCION" << (char)175 << " " << clientes[cliente_id].direccion.DIRECCION << RESET << endl;
     cout << "\t\t\t\t\t\t" << GRAY << (char)186; estetica(7, ' '); cout << LGREEN << "DISTRITO" << (char)175 << " " << clientes[cliente_id].direccion.DISTRITO << RESET << endl;
     cout << "\t\t\t\t\t\t" << GRAY << (char)186; estetica(7, ' '); cout << LGREEN << "PROVINCIA" << (char)175 << " " << clientes[cliente_id].direccion.PROVINCIA << RESET << endl;
-    cout << "\t\t\t\t\t\t" << GRAY << (char)186; estetica(7, ' '); cout << LGREEN << "NUMERO DE CELULAR" << (char)175 << " " << clientes[cliente_id].contacto.CELULAR << RESET << endl;
+    cout << "\t\t\t\t\t\t" << GRAY << (char)186; estetica(7, ' '); cout << LGREEN << "NUMERO DE CELULAR" << (char)175 << " " << clientes[cliente_id].CELULAR << RESET << endl;
     cout << "\t\t\t\t\t\t" << GRAY << (char)186; estetica(7, ' '); cout << CYAN << "HORA DE INGRESO" << (char)175 << " " << clientes[cliente_id].hora_operacion << endl << RESET;
     cout << "\t\t\t\t\t\t" << GRAY << (char)186; estetica(7, ' '); cout << CYAN << "HORA DE INGRESO" << (char)175 << " " << clientes[cliente_id].tiene_sancion << endl << RESET;
     cout << "\t\t\t\t\t\t" << GRAY << (char)186; estetica(7, ' '); cout << CYAN << "HORA DE INGRESO" << (char)175 << " " << clientes[cliente_id].tiempo_sancion << endl << RESET;
@@ -211,35 +195,18 @@ extern void boleta_devolucion(int cliente_id = 0) {
 
 extern void make_comentario(int cliente_id, int comentari_id) {
     system("cls");
-    cout << BLUE << "\t\t\t";  estetica(80, 219);
     cout << endl << endl << endl << endl;
-    cout << LGREEN << "\t\t\t\t\t[!]" << RESET << CYAN << " BIENVENIDO AL APARTADO DE COMENTARIOS " << clientes[cliente_id].informacion_cliente.NOMBRE_APELLIDO<< RESET<<endl;  
+    cout << LGREEN << "\t\t\t\t\t[!]" << RESET << CYAN << " BIENVENIDO AL APARTADO DE COMENTARIOS " << clientes[cliente_id].NOMBRE_APELLIDO<< RESET<<endl;  
     comentarios[comentari_id].comentario_id = comentari_id;
     comentarios[comentari_id].cliente_id = cliente_id;
     string mensaje;
-
-    while (true) {
-        cout << LGREEN << "\t\t\t\t\t[++]" << RESET << CYAN << " INGRESE SU correo electronico: " << RESET;
-        getline(cin, mensaje);
-        if (verif_correo(mensaje)) {
-            clientes[cliente_id].contacto.correo_contacto = mensaje;
-            break;
-        }
-        else {
-            cout << RED << "\n\t\t\t\t[!]" << RESET << ORANGE << " Ingreso de forma erronea su correo\n" << RESET;
-        }
-        
-    }
-    cout << LGREEN << "\t\t\t\t\t[++]" << RESET << CYAN << " INGRESE SU COMENTARIO DEL ULTIMO LIBRO QUE DEVOLVIO: " << RESET;
+    cout << LGREEN << "\t\t\t\t\t[++]" << RESET << CYAN << " INGRESE SU COMERNTARIO DEL ULTIMO LIBRO QUE DEVOLVIO: " << RESET;
     getline(cin, mensaje);
     while (true) {
-        cout << LGREEN << "\t\t\t\t\t[!]" << RESET << CYAN << " cual seria la nota que le pondria al libro? 0-5: " << RESET;
+        cout << LGREEN << "\t\t\t\t\t[!]" << RESET << CYAN << " cual seria la nota que le pondria al libro? 1-5: " << RESET;
         cin >> mensaje;
-        if (verif_entero(mensaje) && stoi(mensaje)>=0 && stoi(mensaje)<=5 ) {
+        if (verif_entero(mensaje) && stoi(mensaje) > 0 && stoi(mensaje) <=5) {
             break;
-        }
-        else{
-            cout << RED << "\t\t\t\t\t[!]" << RESET << ORANGE << " INGRESE UNA CALIFICACION VALIDA!\n" << RESET;
         }
     }
     comentarios[comentari_id].tiempo = capture_time();
